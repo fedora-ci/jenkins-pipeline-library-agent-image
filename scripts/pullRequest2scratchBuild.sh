5b12da6178af9d62fa55250c7cdfd80e6839eba9
Michal Srb <michal@redhat.com>
2020-11-25 21:48:04 +0100

    [pullRequest2scratchBuild] refactoring

--- scripts/pullRequest2scratchBuild.sh
+++ scripts/pullRequest2scratchBuild.sh
@@ -14,26 +14,58 @@
 # are replaced with colons. I.e. "forks/user/rpms/repo" would be encoded as "forks:user:rpms:repo"
 # in the SRPM name.
 
+# Required environment variables:
+# REPO_FULL_NAME - full name of the repository; for example: "rpms/jenkins"
+# REPO_NAME - short name of the repository; for example: "jenkins"
+# RELEASE_ID - release id; for example: "f34"
+# PR_ID - pull request number; for example: 2
+# PR_UID - Pagure's unique pull request id
+# PR_COMMIT - commit hash
+# PR_COMMENT - Pagure's comment id that triggered the testing, 0 if the pull request was just opened
+# SOURCE_REPO_FULL_NAME - full name of the source repository; for example: "forks/user/rpms/repo"
+# KOJI_KEYTAB - path to the keytab that can be used to build SRPMs in Koji
+# KRB_PRINCIPAL - kerberos principal
+
+
+fedpkg_bin=${FEDPKG_BIN:-/usr/bin/fedpkg}
+pagure_url=${PAGURE_URL:-https://src.fedoraproject.org}
+
 set -e
 set -x
 
 
-if [ -d "${REPO_NAME}" ]; then
-    rm -Rf ${REPO_NAME}
-fi
+function cleanup() {
+    # Remove directory, if it exists already
+    local dir_name=${1}
+    if [ -d "${dir_name}" ]; then
+        rm -Rf ${dir_name}
+    fi
+}
 
-# This is a standard pull request; clone it and switch to the pull request branch
-fedpkg clone -a ${REPO_FULL_NAME}
-git fetch https://src.fedoraproject.org/${REPO_FULL_NAME}.git refs/pull/${PR_ID}/head:pr${PR_ID}
-git checkout pr${PR_ID}
 
+function prepare_repository() {
+    # Clone the repository and fetch the pull request changes
+    local repo_full_name=${1}
+    local pr_id=${2}
+    local repo_name=$(basename ${repo_full_name})
+
+    ${fedpkg_bin} clone -a ${repo_name}
+    pushd ${repo_name}
+        git fetch ${pagure_url}/${repo_full_name}.git refs/pull/${pr_id}/head:pr${pr_id}
+        git checkout pr${pr_id}
+    popd
+}
+
+cleanup ${REPO_NAME}
+prepare_repository ${REPO_FULL_NAME} ${PR_ID}
 cd ${REPO_NAME}
 
+# Build SRPM
 srpm_path=$(fedpkg --release ${RELEASE_ID} srpm | grep 'Wrote:' | awk '{ print $2 }')
 srpm_name=$(basename ${srpm_path})
 new_srpm_name="fedora-ci_${PR_UID}_${PR_COMMIT}_${PR_COMMENT};${SOURCE_REPO_FULL_NAME//\//:}.${RELEASE_ID}.src.rpm"
 mv ${srpm_name} ${new_srpm_name}
 
+# Scratch-build the SRPM in Koji
 kinit -k -t ${KOJI_KEYTAB} ${KRB_PRINCIPAL}
-
-fedpkg scratch-build --nowait --target ${RELEASE_ID} --srpm ${new_srpm_name}
+${fedpkg_bin} scratch-build --nowait --target ${RELEASE_ID} --srpm ${new_srpm_name}
