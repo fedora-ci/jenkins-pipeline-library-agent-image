5daf18687a9b154fd7cb40dbf8ac708ddb447b22
Michal Srb <michal@redhat.com>
2021-01-26 13:16:14 +0100

    [scratch.sh] Store koji URL in a file

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -13,13 +13,18 @@ if [ $# -ne 4 ]; then
     exit 101
 fi
 
+koji_log=${workdir}/koji.log
+koji_url=${workdir}/koji_url
+
 set -e
 set -x
 
+rm -f ${fedpkg_log}
+rm -f ${koji_url}
+
 profile=${1}
-wait_method=${2}
-target=${3}
-source_url=${4}
+target=${2}
+source_url=${3}
 
 if [ -z "${KOJI_KEYTAB}" ]; then
     echo "Missing keytab, cannot continue..."
@@ -33,4 +38,11 @@ fi
 
 kinit -k -t ${KOJI_KEYTAB} ${KRB_PRINCIPAL}
 
-koji -p ${profile} build --scratch ${ARCH_OVERRIDE:+--arch-override=$ARCH_OVERRIDE} --${wait_method} ${target} ${source_url}
+koji -p ${profile} build --scratch ${ARCH_OVERRIDE:+--arch-override=$ARCH_OVERRIDE} --nowait ${target} ${source_url} > ${koji_log}
+cat ${koji_log}
+
+cat ${koji_log} | grep '^Task info: ' | awk '{ print $3 }' > ${koji_url}
+
+task_id=$(cat ${koji_log} | grep '^Created task: ' | awk '{ print $3 }')
+
+koji -p ${profile} watch-task ${task_id}
